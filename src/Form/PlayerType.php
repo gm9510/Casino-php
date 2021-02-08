88aca3c2acc1ef2f6f7663c88f8f85f6f68d35a5
Gustavo Quijote <gm9510@gmail.com>
2021-02-08 17:16:16 -0500

    Los formularios del crud de player ahora usan eventListener para generar los campos dinamicamente

--- src/Form/PlayerType.php
+++ src/Form/PlayerType.php
@@ -6,16 +6,34 @@ use App\Entity\Player;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class PlayerType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('Name');
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder->addEventListener(FormEvents::PRE_SET_DATA,
+                    function (FormEvent $event){
+                        $player = $event->getData();
+                        $form = $event->getForm();
+                        if( !$player || null === $player->getId() ){
+                            $form->add('Name');
+                        }
+                    }
+                )
+                ->addEventListener(FormEvents::PRE_SET_DATA,
+                    function (FormEvent $event){
+                        $player = $event->getData();
+                        $form = $event->getForm();
+                        if( null !== $player->getId() ){
+                            $form->add('Money');
+                        }
+                    }
+        );
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
+
+    public function configureOptions(OptionsResolver $resolver){
         $resolver->setDefaults([
             'data_class' => Player::class,
         ]);
